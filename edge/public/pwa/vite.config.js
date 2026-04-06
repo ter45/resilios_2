34aed56ce066594832f2e30f6b04e5816da1e152
JESUS TELMO NENE <neneter45@gmail.com>
2026-04-06 14:47:39 -0500

    Actulizo vite

--- edge/public/pwa/vite.config.js
+++ edge/public/pwa/vite.config.js
@@ -51,11 +51,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true
       },
       '/health': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true
       }
     }
